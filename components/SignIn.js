6c03f1088841a1cf6fa8b0e78b745c098149812c
Zeriab Chiah <zeriab@hotmail.com>
2022-12-16 23:53:20 +0200

    sign in c

---
+++ components/SignIn.js
@@ -0,0 +1,124 @@
+import React, { useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import {
+    StyleSheet,
+    Alert,
+    Text,
+    View,
+    Image,
+    TextInput,
+    Button,
+    TouchableOpacity,
+  } from "react-native";
+import { connect } from 'react-redux'
+
+export function SignIn() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+  
+  function login() {
+    fetch("http://localhost:8080/user/signin", {
+      method: 'POST',
+      headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+          email: email,
+          password: password,
+      })
+  })
+
+  .then(response => response.json())
+  .then(responseData => {
+    console.log(responseData)
+}) .catch((error) => {
+          console.error(error);
+          Alert.alert('invalid data');
+      });
+  }
+  const forgetPassword=()=>{
+    console.log('forget password')
+  }
+const signup=()=>{
+
+}
+
+    return (
+        <View style={styles.container}>
+     <StatusBar style="auto" />
+        <View style={styles.inputView}>
+          <TextInput
+            style={styles.TextInput}
+            placeholder="Email."
+            placeholderTextColor="#003f5c"
+            onChangeText={(email) => setEmail(email)}
+          /> 
+        </View> 
+        <View style={styles.inputView}>
+          <TextInput
+            style={styles.TextInput}
+            placeholder="Password."
+            placeholderTextColor="#003f5c"
+            secureTextEntry={true}
+            onChangeText={(password) => setPassword(password)}
+          /> 
+        </View>
+       
+        <TouchableOpacity style={styles.loginBtn} onPress={()=>{login()}}>
+          <Text style={styles.loginText}>LOGIN</Text> 
+        </TouchableOpacity> 
+        <TouchableOpacity style={styles.registerBtn} onPress={()=>{signup()}}>
+          <Text >Register</Text> 
+        </TouchableOpacity> 
+        <TouchableOpacity style={styles.forgot_button} onPress={()=>{forgetPassword()}}>
+          <Text >Forgot Password?</Text> 
+        </TouchableOpacity> 
+        
+      </View>
+    )
+  }
+
+
+const styles = StyleSheet.create({
+    TextInput: {
+      height: 40,
+      margin: 12,
+      borderWidth: 1,
+      padding: 10,
+    },
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    registerBtn: {
+      width: "80%",
+      borderRadius: 25,
+      height: 50,
+      alignItems: "center",
+      justifyContent: "center",
+      marginTop: 40,
+      backgroundColor: "#F4B910",
+    },
+    loginBtn: {
+      width: "80%",
+      borderRadius: 25,
+      height: 50,
+      alignItems: "center",
+      justifyContent: "center",
+      marginTop: 40,
+      backgroundColor: "#3575ED",
+    },
+    forgot_button: {
+      width: "80%",
+      borderRadius: 25,
+      height: 50,
+      alignItems: "center",
+      justifyContent: "center",
+      marginTop: 40,
+      backgroundColor: "#DCF410",
+    },
+  });
+
