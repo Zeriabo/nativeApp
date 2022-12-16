c2a7db3a6fbff2b9761fdafacbd927481aaf842b
Zeriab Chiah <zeriab@hotmail.com>
2022-12-16 23:52:54 +0200

    signin

--- App.js
+++ App.js
@@ -2,105 +2,22 @@ import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
 import {
   StyleSheet,
-  Text,
   View,
-  Image,
-  TextInput,
-  Button,
-  TouchableOpacity,
+
 } from "react-native";
+import { SignIn } from "./components/SignIn";
 
 export default function App() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
 
-  function validateForm() {
-    return email.length > 0 && password.length > 0;
-  }
-  function handleSubmit(event) {
-    event.preventDefault();
-  }
-const reset=()=>{
-  let textInput = this.refs["usernameText"];
-  console.log(textInput);
-  let passwordInput = this.refs["passwordText"];
-  passwordInput.clear
-  textInput.clear();
-}
+
+
   return (
-<View style={styles.container}>
-      <Image style={styles.image} height={1} source={require("./assets/icon.png")} /> 
-      <StatusBar style="auto" />
-      <View style={styles.inputView}>
-        <TextInput
-          style={styles.TextInput}
-          placeholder="Email."
-          placeholderTextColor="#003f5c"
-          onChangeText={(email) => setEmail(email)}
-        /> 
-      </View> 
-      <View style={styles.inputView}>
-        <TextInput
-          style={styles.TextInput}
-          placeholder="Password."
-          placeholderTextColor="#003f5c"
-          secureTextEntry={true}
-          onChangeText={(password) => setPassword(password)}
-        /> 
-      </View>
-     
-      <TouchableOpacity style={styles.loginBtn}>
-        <Text style={styles.loginText}>LOGIN</Text> 
-      </TouchableOpacity> 
-      <TouchableOpacity style={styles.registerBtn}>
-        <Text >Register</Text> 
-      </TouchableOpacity> 
-      <TouchableOpacity style={styles.forgot_button}>
-        <Text >Forgot Password?</Text> 
-      </TouchableOpacity> 
+
+   <SignIn/>
       
-    </View>
+
   );
 }
 
-const styles = StyleSheet.create({
-  TextInput: {
-    height: 40,
-    margin: 12,
-    borderWidth: 1,
-    padding: 10,
-  },
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  registerBtn: {
-    width: "80%",
-    borderRadius: 25,
-    height: 50,
-    alignItems: "center",
-    justifyContent: "center",
-    marginTop: 40,
-    backgroundColor: "#F4B910",
-  },
-  loginBtn: {
-    width: "80%",
-    borderRadius: 25,
-    height: 50,
-    alignItems: "center",
-    justifyContent: "center",
-    marginTop: 40,
-    backgroundColor: "#3575ED",
-  },
-  forgot_button: {
-    width: "80%",
-    borderRadius: 25,
-    height: 50,
-    alignItems: "center",
-    justifyContent: "center",
-    marginTop: 40,
-    backgroundColor: "#DCF410",
-  },
-});
+
+
