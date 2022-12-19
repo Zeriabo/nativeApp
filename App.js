28052a8f0a32c1783df7c0873cf8874f3c70a0c4
Zeriab Chiah <zeriab@hotmail.com>
2022-12-19 12:26:15 +0200

    app

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 import {
   StyleSheet,
   View,
-
 } from "react-native";
 import { SignIn } from "./components/SignIn";
 
@@ -12,12 +13,28 @@ export default function App() {
 
 
   return (
-
-   <SignIn/>
+<Provider>
+<PersistGate>
+<View style={styles.container}>
+<SignIn/>
+   </View>
+</PersistGate>
+</Provider>
+   
       
 
   );
 }
+const styles = StyleSheet.create({
 
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+
+  }
+}
+);
 
 
