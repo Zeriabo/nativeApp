8206edd22f96983097de33a852aef35c60153e11
Zeriab Chiah <zeriab@hotmail.com>
2022-12-21 14:49:29 +0200

    home

--- App.js
+++ App.js
@@ -4,16 +4,20 @@ import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { StyleSheet, View } from "react-native";
 import { SignIn } from "./components/SignIn";
+import { Home } from "./components/Home";
 import store from "./state/store/Store";
 import persistor from "./state/store/Store";
 import { Loading } from "./components/Loading";
+import { NavigationContainer } from "@react-navigation/native";
 export default function App() {
   return (
-    <Provider store={store}>
-      <View style={styles.container}>
-        <SignIn />
-      </View>
-    </Provider>
+    <NavigationContainer>
+      <Provider store={store}>
+        <View style={styles.container}>
+          <Home />
+        </View>
+      </Provider>
+    </NavigationContainer>
   );
 }
 const styles = StyleSheet.create({
---
+++ components/Home.js
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Alert,
+  Text,
+  View,
+  Image,
+  TextInput,
+  Button,
+  TouchableOpacity,
+} from "react-native";
+import { connect } from "react-redux";
+import { SignIn } from "./SignIn";
+
+export function Home() {
+  return (
+    <View style={styles.container}>
+      <SignIn />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  TextInput: {
+    width: 200,
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  registerBtn: {
+    width: 200,
+    borderRadius: 25,
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 40,
+    backgroundColor: "#F4B910",
+  },
+  loginBtn: {
+    width: 200,
+    borderRadius: 25,
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 40,
+    backgroundColor: "#3575ED",
+  },
+  forgot_button: {
+    width: 200,
+    borderRadius: 25,
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 40,
+    backgroundColor: "#DCF410",
+  },
+});
