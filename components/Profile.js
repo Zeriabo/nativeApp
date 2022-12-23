b083bf39e97eceea8685a6d1e40b91e41feeabc0
Zeriab Chiah <zeriab@hotmail.com>
2022-12-24 00:34:37 +0200

    Profile

--- components/Profile.js
+++ components/Profile.js
@@ -17,12 +17,64 @@ export function Profile({ route, navigation }) {
   console.log(route.params.responseData);
   return (
     <View style={styles.container}>
-      <Text>Profile</Text>
-      <Text>{route.params.responseData.email}</Text>
-      <Text>{route.params.responseData.name}</Text>
-      <Text>{route.params.responseData.dateofbirth}</Text>
+      <Text>Hello {route.params.responseData.name}</Text>
+      <TouchableOpacity
+        style={styles.loginBtn}
+        onPress={() => login()}
+        title="Inbox"
+      >
+        <Text>Inbox</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.loginBtn}
+        onPress={() => login()}
+        title="Send a message"
+      >
+        <Text>Send message</Text>
+      </TouchableOpacity>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  TextInput: {
+    width: 200,
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    paddingTop: 30,
+    paddingLeft: 20,
+  },
+  registerBtn: {
+    width: 200,
+    borderRadius: 25,
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 40,
+    backgroundColor: "#F4B910",
+  },
+  loginBtn: {
+    width: 200,
+    borderRadius: 25,
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 40,
+    backgroundColor: "#3575ED",
+  },
+  forgot_button: {
+    width: 200,
+    borderRadius: 25,
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 40,
+    backgroundColor: "#DCF410",
+  },
+});
