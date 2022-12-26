2f0e2232e7ec63eff2de5476b31d5f2598c5e7f6
Zeriab Chiah <zeriab@hotmail.com>
2022-12-26 16:57:32 +0200

    inbox

--- components/Inbox.js
+++ components/Inbox.js
@@ -9,46 +9,58 @@ import {
   TextInput,
   Button,
   TouchableOpacity,
+  FlatList,
 } from "react-native";
 import { connect } from "react-redux";
 
 export function Inbox({ route }) {
+  const [selectedId, setSelectedId] = useState(null);
+  const renderItem = ({ item }) => {
+    const backgroundColor = item.id === selectedId ? "#6e3b6e" : "#f9c2ff";
+    const color = item.id === selectedId ? "white" : "black";
+
+    return (
+      <Item
+        item={item}
+        onPress={() => setSelectedId(item.id)}
+        backgroundColor={{ backgroundColor }}
+        textColor={{ color }}
+      />
+    );
+  };
+
+  const Item = ({ item, onPress, backgroundColor, textColor }) => (
+    <TouchableOpacity onPress={onPress} style={[styles.item, backgroundColor]}>
+      <Text style={styles.sender}>
+        {item.datetime} {item.sender} {item.title}
+      </Text>
+    </TouchableOpacity>
+  );
+
   const messages = route.params;
+
   return (
     <View style={styles.container}>
-      {messages.map((elem) => {
-        return (
-          <View
-            style={{
-              width: 200,
-              height: 50,
-
-              flexDirection: "row",
-            }}
-          >
-            <Text>
-              {elem.datetime}:{elem.sender}
-            </Text>
-            <Text>{elem.title}</Text>
-          </View>
-        );
-      })}
+      <FlatList
+        data={messages}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    marginTop: 50,
-    backgroundColor: "powderblue",
-    width: "auto",
+    flex: 1,
+    marginTop: StatusBar.currentHeight || 0,
   },
-  bigBlue: {
-    color: "blue",
-    fontWeight: "bold",
-    fontSize: 30,
+  item: {
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
   },
-  red: {
-    color: "red",
+  title: {
+    fontSize: 32,
   },
 });
